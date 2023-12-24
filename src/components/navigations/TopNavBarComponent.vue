34c659f2c0285228512101421fbbd64b3bcd3b5b
Weberworld <omotosoboluwatife23@gmailcom>
2023-12-24 19:58:34 +0100

    intial

--- src/components/navigations/TopNavBarComponent.vue
+++ src/components/navigations/TopNavBarComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="relative container mx-auto p-6">
-        <div class="flex items-center justify-between">
+        <div class="flex justify-between">
             <div class="pt-2 text-left">
                 <HeaderTextComponent :text="'FarmCI'" :color="'light'" />
             </div>
@@ -9,9 +9,9 @@
             <!-- Large Screen Nav items -->
             <div class="hidden sm:flex justify-between ">
                 <div class="hidden sm:flex space-x-3">
-                    <a v-for="item in nav_item_list" :key="item"
+                    <a v-for="item in nav_item_list" :key="item" :href="item.id"
                         class="text-slate-200 font-semibold py-1 px-3 hover:bg-amber-500 hover:text-white hover:cursor-pointer">
-                        {{ item }}
+                        {{ item.name }}
                     </a>
                 </div>
 
@@ -31,12 +31,9 @@
                     <div class="absolute translate-x-30 ease-linear duration-1000 top-20 right-0 rounded-lg w-full h-screen p-8 shadow-inner shadow-green-900"
                         :class="[AppColors.bg_white, AppColors.text_success]">
                         <ul class="absolute list-disc list-inside space-y-6 text-left justify-baseline mt-12 h-auto">
-                            <li><a href="#hero" class="font-bold text-2xl">Home</a></li>
-                            <li><a href="#about" class="font-bold text-2xl">About</a></li>
-                            <li><a href="#services" class="font-bold text-2xl">Service</a></li>
-                            <li><a href="#partners" class="font-bold text-2xl">Partners</a></li>
-                            <li><a href="#features" class="font-bold text-2xl">Features</a></li>
-                            <li><a href="#contact" class="font-bold text-2xl">Contact</a></li>
+                            <li v-for="item in nav_item_list" :key="item">
+                                <a :href="item.id" class="font-bold text-2xl">{{ item.name }}</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -62,23 +59,27 @@ import MenuIconComponent from "@/components/utils/MenuIconComponent.vue/";
 const nav_item_list = ref([
     {
         name: "Home",
-        id: "#hero," 
+        id: "#hero" 
     },
     {
         name: "About",
-        id: "#about," 
+        id: "#about" 
     },
     {
         name: "Service",
-        id: "#service," 
+        id: "#services" 
     },
     {
         name: "Features",
-        id: "#features," 
+        id: "#features" 
+    },
+    {
+        name: "Partners",
+        id: "#partners" 
     },
     {
         name: "Contact",
-        id: "#contact," 
+        id: "#contact" 
     },
 ])
 const show_mobile = ref(false);
